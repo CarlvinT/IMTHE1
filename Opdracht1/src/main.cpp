bcb65c77dc26f21b03671a8c63a7807e2a5a181b
Carlvin <Carlvintromp@hotmail.com>
2018-09-07 16:44:58 +0200

    Opdracht 2

--- Opdracht1/src/main.cpp
+++ Opdracht1/src/main.cpp
@@ -2,23 +2,29 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
+void setup(){
+  // Define Pin and data bank
+  DDRD = 0b00100000; //DDR + Data bank letter = 0b000100000 set pin number to 1 that is gonna be active
+}
+
+void blinks(){
+  PORTD = 0b00100000; // Set pin pb5 to high with data bank B
+  _delay_ms(1000); //Wacht 2 seconden
+  PORTD = 0b00000000; //Set pin to LOW with data bank B
+  _delay_ms(1000);
+}
 
 // Main function so program can run
 int main(void){
-
-    // Define Pin and data bank
-    DDRB = 0b000100000; //DDR + Data bank letter = 0b000100000 set pin number to 1 that is gonna be active
+    // Run code that only needs to be run once (setup)
+    setup();
 
     // Same as Loop() in adruino. Infite loop while 1.
     while(1){
-        //
-        PORTB = 0b000100000; // Set pin pb5 to high with data bank B
-        _delay_ms(2000); //Wacht 2 seconden
-        PORTB = 0b000000000; //Set pin to LOW with data bank B
-        _delay_ms(2000);
+      blinks();
     }
 
-    // Return 0 cause maint is int
+    // Return 0 cause main is int
     return 0;
 
 }
