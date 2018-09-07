666c44b8a8327c271279c3042fb5c1882fe704fa
Carlvin <Carlvintromp@hotmail.com>
2018-09-07 19:48:53 +0200

    Opdracht 2

--- Opdracht 2/src/main.cpp
+++ Opdracht 2/src/main.cpp
@@ -1,9 +1,19 @@
-#include <Arduino.h>
+#include <avr/io.h>
+#include <util/delay.h>
 
-void setup() {
-    // put your setup code here, to run once:
+void opstelling(){
+  
 }
 
-void loop() {
-    // put your main code here, to run repeatedly:
+int main(void){
+    // Run code that only needs to be run once (setup)
+    opstelling();
+
+    // Same as Loop() in adruino. Infite loop while 1.
+    while(1){
+
+    }
+
+    // Return 0 cause main is int
+    return 0;
 }
--- Opdracht1/src/main.cpp
+++ Opdracht1/src/main.cpp
@@ -2,22 +2,22 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
-void setup(){
+void opstelling(){
   // Define Pin and data bank
-  DDRD = 0b00100000; //DDR + Data bank letter = 0b000100000 set pin number to 1 that is gonna be active
+  DDRD = 0b00100000;
 }
 
 void blinks(){
-  PORTD = 0b00100000; // Set pin pb5 to high with data bank B
-  _delay_ms(1000); //Wacht 2 seconden
-  PORTD = 0b00000000; //Set pin to LOW with data bank B
+  PORTD = 0b00100000;
+  _delay_ms(1000); // Delay by 1 second
+  PORTD = 0b00000000;
   _delay_ms(1000);
 }
 
 // Main function so program can run
 int main(void){
     // Run code that only needs to be run once (setup)
-    setup();
+    opstelling();
 
     // Same as Loop() in adruino. Infite loop while 1.
     while(1){
@@ -26,5 +26,4 @@ int main(void){
 
     // Return 0 cause main is int
     return 0;
-
 }
